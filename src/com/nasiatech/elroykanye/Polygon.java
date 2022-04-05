0b8acc7f6cf99dbce94c3af339e8da5c4fe9e1ad
Elroy Kanye <elroykanye@gmail.com>
2022-04-05 09:40:06 +0100

    add a polygon interface

---
+++ src/com/nasiatech/elroykanye/Polygon.java
@@ -0,0 +1,21 @@
+package com.nasiatech.elroykanye;
+
+/**
+ * @author Elroy Kanye
+ * @version 1.0
+ * @since 1.0
+ * Polygon class
+ */
+public interface Polygon {
+    /**
+     * This evaluates the area of the polygon
+     * @return the area of the polygon
+     */
+    double getArea();
+
+    /**
+     * This evaluates the perimeter of the polygon
+     * @return the perimeter of the polygon
+     */
+    double getPerimeter();
+}
